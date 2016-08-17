c2b33a520928f581586a002105dfe890a04416d9
Jean Paul Valle Rodriguez <jpvalle@espol.edu.ec>
2016-08-16 21:06:28 -0500

    Multiventana

--- VisualGUI/VisualGUI/winAdmin.xaml.vb
+++ VisualGUI/VisualGUI/winAdmin.xaml.vb
@@ -1,3 +1,6 @@
 ﻿Public Class winAdmin
-
+    Private Sub winAdmin_Closed(sender As Object, e As EventArgs) Handles MyBase.Closed, MyBase.Closed
+        Me.Hide()
+        Me.Owner.Show()
+    End Sub
 End Class
--- VisualGUI/VisualGUI/winVendedor.xaml.vb
+++ VisualGUI/VisualGUI/winVendedor.xaml.vb
@@ -1,3 +1,6 @@
 ﻿Public Class winVendedor
-
+    Private Sub Window_Closed(sender As Object, e As EventArgs)
+        Me.Hide()
+        Me.Owner.Show()
+    End Sub
 End Class
