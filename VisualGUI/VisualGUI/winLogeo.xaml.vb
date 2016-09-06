1707f648f72adacf11ffbb072cecc156db2b437c
Kimmy <kimmy_11_07@hotmail.com>
2016-09-06 01:36:01 -0500

    BD en login

--- VisualGUI/VisualGUI/winLogeo.xaml.vb
+++ VisualGUI/VisualGUI/winLogeo.xaml.vb
@@ -1,39 +1,52 @@
 ﻿Imports System.Data
-
+Imports System.Data.OleDb
 Class winLogeo
     Private Sub btnAceptar_Click(sender As Object, e As RoutedEventArgs) Handles btnAceptar.Click
-        Dim path As String = "..\..\admin.xml"
+        Dim path As String = "..\..\..\BDEmpresa.accdb"
+        Dim dbPath As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & path
         Dim flag As String = ""
-        Dim dsUsuarios As New DataSet
-        dsUsuarios.ReadXml(path)
-        For Each fila As DataRow In dsUsuarios.Tables("Admin").Rows
-            If (Me.txtUser.Text.Equals(fila(0)) AndAlso Me.txtPass.Password.Equals(fila(1))) Then
-                flag = "a"
+        Using dbConexion As New OleDbConnection(dbPath)
+            Dim sentencia As String = "Select * from Usuarios"
+            Dim dbAdapter As New OleDbDataAdapter(New OleDbCommand(sentencia, dbConexion))
+            Dim dsUsuarios As New DataSet
+            dbAdapter.Fill(dsUsuarios, "Usuarios")
 
+
+            For Each fila As DataRow In dsUsuarios.Tables("Usuarios").Rows
+
+                If (Me.txtUser.Text.Equals(fila(1)) AndAlso Me.txtPass.Password.Equals(fila(2))) Then
+                    'MessageBox.Show(fila("Rol"))
+                    If fila("Rol") = "Admin" Then
+                        flag = "a"
+
+                    End If
+                    If fila("Rol") = "Vendedor" Then
+                        flag = "v"
+
+                    End If
+                    Exit For
+                End If
+            Next
+
+            If (flag.Equals("a")) Then
+
+                Dim winAdmin As New winAdmin
+                winAdmin.Owner = Me
+                winAdmin.Show()
+                Me.Hide()
+
+            ElseIf (flag.Equals("v")) Then
+
+                Dim winVendedor As New winVendedor
+                winVendedor.Owner = Me
+                winVendedor.Show()
+                Me.Hide()
+            Else
+                MessageBox.Show("Usuario o contraseña incorrecta! TE AMO")
             End If
-        Next
-        For Each filaV As DataRow In dsUsuarios.Tables("Vendedor").Rows
-            If (Me.txtUser.Text.Equals(filaV(0)) AndAlso Me.txtPass.Password.Equals(filaV(1))) Then
-                flag = "v"
+        End Using
 
-            End If
-        Next
-        If (flag.Equals("a")) Then
 
-            Dim winAdmin As New winAdmin
-            winAdmin.Owner = Me
-            winAdmin.Show()
-            Me.Hide()
-
-        ElseIf (flag.Equals("v")) Then
-
-            Dim winVendedor As New winVendedor
-            winVendedor.Owner = Me
-            winVendedor.Show()
-            Me.Hide()
-        Else
-            MessageBox.Show("Usuario o contraseña incorrecta! TE AMO")
-        End If
     End Sub
 
     Private Sub btnCerrar_Click(sender As Object, e As RoutedEventArgs) Handles btnCerrar.Click
